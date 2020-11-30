a35cc762c151a42c96c7847c476f3bbf301431b4
nostam <matsonho@gmail.com>
2020-11-30 12:32:58 +0100

    fix styling for icons in different screen size

--- src/components/AppNavBar.jsx
+++ src/components/AppNavBar.jsx
@@ -7,6 +7,7 @@ import {
   Nav,
   InputGroup,
   Col,
+  Row,
 } from "react-bootstrap";
 import { withRouter, Link, NavLink } from "react-router-dom";
 import { IconContext } from "react-icons";
@@ -25,93 +26,97 @@ class AppNavBar extends React.Component {
   render() {
     return (
       <Navbar bg="white" variant="light" className="py-0">
-        <Container>
-          <Navbar.Brand as={Link} to="/" className="navbarBrand mr-1">
-            <IconContext.Provider
-              value={{
-                size: "38px",
-                className: "LinkedinIcon",
-                color: "#0a66c2",
-                title: "LinkedIn",
-              }}
-            >
-              <FaLinkedin />
-            </IconContext.Provider>
-          </Navbar.Brand>
-          <Form inline className="mr-auto ml-0 pl-0">
-            <InputGroup>
-              <InputGroup.Prepend>
-                <InputGroup.Text>
-                  <IconContext.Provider
-                    value={{
-                      size: "1rem",
-                      className: "SearchIcon",
-                      color: "grey",
-                      backgroundColor: "#60627c",
-                    }}
-                  >
-                    <FaSearch />
-                  </IconContext.Provider>
-                </InputGroup.Text>
-              </InputGroup.Prepend>
-              <FormControl
-                type="text"
-                placeholder="Search"
-                className="mr-sm-2"
-              />
-            </InputGroup>
-          </Form>
-          <Nav.Link className="px-1">
-            <Col className="navCol">
-              <FaHome className="navIcon" />
-              <span className="d-none d-lg-block navIconText">Home</span>
-            </Col>
-          </Nav.Link>
-          <Nav.Link className="px-1">
-            <Col className="navCol">
-              <BsPeopleFill className="navIcon" />
-              <span className="d-none d-lg-block navIconText">My Network</span>
-            </Col>
-          </Nav.Link>
-          <Nav.Link className="px-1">
-            <Col className="navCol">
-              <GiHandBag className="navIcon" />{" "}
-              <span className="d-none d-lg-block navIconText">Jobs</span>
-            </Col>
-          </Nav.Link>
-          <Nav.Link className="px-1">
-            <Col className="navCol">
-              <RiMessage2Fill className="navIcon" />
-              <span className="d-none d-lg-block navIconText">Messaging</span>
-            </Col>
-          </Nav.Link>
-          <Nav.Link className="px-1">
-            <Col className="navCol">
-              <FaBell className="navIcon" />
-              <span className="d-none d-lg-block navIconText">
-                Notifications
-              </span>
-            </Col>
-          </Nav.Link>
-          <Nav.Link className="px-1">
-            <Col className="navCol">
-              <FaUserCircle className="navIcon" />
-              <span className="d-none d-lg-block navIconText">Me</span>
-            </Col>
-          </Nav.Link>{" "}
-          <Nav.Link className="px-1">
-            <Col className="navCol">
-              <BsGrid3X3Gap className="navIcon" />
-              <span className="d-none d-lg-block navIconText">Work</span>
-            </Col>
-          </Nav.Link>
-          <Nav.Link className="px-1">
-            <Col className="navCol">
-              <BsCollectionPlay className="navIcon" />
-              <span className="d-none d-lg-block navIconText">Learning</span>
-            </Col>
-          </Nav.Link>
-        </Container>
+        <Col lg={{ span: "8", offset: "2" }}>
+          <Row className="d-flex flex-nowrap">
+            <Navbar.Brand as={Link} to="/" className="navbarBrand mr-1">
+              <IconContext.Provider
+                value={{
+                  size: "38px",
+                  className: "LinkedinIcon",
+                  color: "#0a66c2",
+                  title: "LinkedIn",
+                }}
+              >
+                <FaLinkedin />
+              </IconContext.Provider>
+            </Navbar.Brand>
+            <Form inline className="navSearch">
+              <InputGroup>
+                <InputGroup.Prepend>
+                  <InputGroup.Text>
+                    <IconContext.Provider
+                      value={{
+                        size: "1rem",
+                        className: "SearchIcon",
+                        color: "grey",
+                        backgroundColor: "#60627c",
+                      }}
+                    >
+                      <FaSearch />
+                    </IconContext.Provider>
+                  </InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl
+                  type="text"
+                  placeholder="Search"
+                  className="mr-sm-2"
+                />
+              </InputGroup>
+            </Form>
+            <Nav.Link className="px-1">
+              <Col className="navCol">
+                <FaHome className="navIcon" />
+                <span className="d-none d-lg-block navIconText">Home</span>
+              </Col>
+            </Nav.Link>
+            <Nav.Link className="px-1">
+              <Col className="navCol">
+                <BsPeopleFill className="navIcon" />
+                <span className="d-none d-lg-block navIconText">
+                  My Network
+                </span>
+              </Col>
+            </Nav.Link>
+            <Nav.Link className="px-1">
+              <Col className="navCol">
+                <GiHandBag className="navIcon" />{" "}
+                <span className="d-none d-lg-block navIconText">Jobs</span>
+              </Col>
+            </Nav.Link>
+            <Nav.Link className="px-1">
+              <Col className="navCol">
+                <RiMessage2Fill className="navIcon" />
+                <span className="d-none d-lg-block navIconText">Messaging</span>
+              </Col>
+            </Nav.Link>
+            <Nav.Link className="px-1">
+              <Col className="navCol">
+                <FaBell className="navIcon" />
+                <span className="d-none d-lg-block navIconText">
+                  Notifications
+                </span>
+              </Col>
+            </Nav.Link>
+            <Nav.Link className="px-1">
+              <Col className="navCol">
+                <FaUserCircle className="navIcon" />
+                <span className="d-none d-lg-block navIconText">Me</span>
+              </Col>
+            </Nav.Link>{" "}
+            <Nav.Link className="px-1">
+              <Col className="navCol">
+                <BsGrid3X3Gap className="navIcon" />
+                <span className="d-none d-lg-block navIconText">Work</span>
+              </Col>
+            </Nav.Link>
+            <Nav.Link className="px-1">
+              <Col className="navCol">
+                <BsCollectionPlay className="navIcon" />
+                <span className="d-none d-lg-block navIconText">Learning</span>
+              </Col>
+            </Nav.Link>
+          </Row>
+        </Col>
       </Navbar>
     );
   }
