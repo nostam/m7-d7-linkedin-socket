35855993cb2481dd68f6001543f01e221751afdd
Lidia Kovac <lidia.kovac1998@gmail.com>
2020-12-01 10:11:48 +0100

    adds experience card

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,1 +1,19 @@
-import React from `react`
+import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../App.css"
+import { Card } from "react-bootstrap"
+
+class Experience extends React.Component { 
+render() {
+    return(
+        <Card style={{ borderRadius: "1vw" }}>
+
+                <Card.Body>
+                    <div id="expTitle" className="info">Experience</div>
+                    <div class="infobody">{this.props.bio}</div>
+                </Card.Body>
+            </Card>
+    )
+}
+
+} export default Experience
