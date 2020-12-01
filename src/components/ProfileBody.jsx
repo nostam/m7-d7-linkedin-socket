623468cf43c2d485d71ab63b33f8984267cea620
nostam <matsonho@gmail.com>
2020-12-01 14:08:40 +0100

    add error checking and spinner to profile body

--- src/components/ProfileBody.jsx
+++ src/components/ProfileBody.jsx
@@ -1,59 +1,65 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Spinner } from "react-bootstrap";
 import Bio from "./BioCard";
 import Header from "./Jumbotron";
 import Sidebar from "./Sidebar";
 
 class Body extends React.Component {
   state = {
-    profile: {
-      name: "",
-      surname: "",
-      propic: "",
-      title: "",
-      area: "",
-      bio: "",
-    },
+    profile: {},
     showAlert: null,
     success: false,
+    loading: true,
   };
 
   componentDidMount() {
-    let response = fetch(
-      "https://striveschool-api.herokuapp.com/api/profile/me",
-      {
-        method: "GET",
-        headers: new Headers({
-          Authorization: process.env.REACT_APP_TOKEN,
-          ContentType: "application/json",
-        }),
-      }
-    )
-      .then((response) => response.json())
+    fetch("https://striveschool-api.herokuapp.com/api/profile/me", {
+      method: "GET",
+      headers: new Headers({
+        Authorization: process.env.REACT_APP_TOKEN,
+        ContentType: "application/json",
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
       .then((info) => {
-        console.log(info);
+        console.log("fetched profile", info);
         let profile = { ...info };
-        this.setState({ profile: profile }, console.log(this.state));
+        this.setState({ profile: profile, loading: false });
+      })
+      .catch((error) => {
+        console.log(error);
       });
   }
   render() {
     return (
       <Container>
-        <Row>
-          <Col xs={12} md={8}>
-            <Header
-              name={this.state.profile.name + " " + this.state.profile.surname}
-              desc={this.state.profile.title}
-              src={this.state.profile.image}
-              loc={this.state.profile.area}
-            />
-            <Bio bio={this.state.profile.bio} />
-          </Col>
-          <Col>
-            {" "}
-            <Sidebar />{" "}
-          </Col>
-        </Row>
+        {this.state.loading ? (
+          <Row className="d-flex justify-content-center my-5">
+            <h3>Loading profile...</h3>
+            <Spinner animation="border" variant="secondary"></Spinner>
+          </Row>
+        ) : (
+          <Row>
+            <Col xs={12} md={8}>
+              <Header
+                name={
+                  this.state.profile.name + " " + this.state.profile.surname
+                }
+                desc={this.state.profile.title}
+                src={this.state.profile.image}
+                loc={this.state.profile.area}
+              />
+              <Bio bio={this.state.profile.bio} />
+            </Col>
+            <Col>
+              <Sidebar />
+            </Col>
+          </Row>
+        )}
       </Container>
     );
   }
