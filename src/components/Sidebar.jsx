755b1db8fe597b19548347317827b16d4f464777
nostam <matsonho@gmail.com>
2020-12-01 14:45:06 +0100

    allocate sidebar css correctly

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Row } from "react-bootstrap";
-import "./Sidebar.css";
+import "./styles/Sidebar.css";
 
 class Sidebar extends Component {
   state = {
