619bb109db80387dc87aaeeaef3686d6f7e1aaf5
nostam <matsonho@gmail.com>
2020-12-02 14:15:54 +0100

    add some button to login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,26 +1,33 @@
 import React, { Component } from "react";
-import { Col, Form } from "react-bootstrap";
+import { Col, Form, Button, Container } from "react-bootstrap";
 import FooterLogo from "../footer_logo.svg";
 import "../styles/Login.css";
 export default class Login extends Component {
   state = {
     user: [],
+    hidden: true,
   };
   onChangeHandler = (e) => {
     this.setState({
       user: { ...this.state.user, [e.target.id]: e.currentTarget.value },
     });
   };
+  toggleShow = (e) => {
+    e.preventDefault();
+    this.setState({ hidden: !this.state.hidden });
+  };
   render() {
     return (
-      <Col md={10} className="loginContainer mt-5 mx-auto">
-        <img src={FooterLogo} className="my-4 py-2" />
-        <h4>Welcome Back</h4>
-        <h6>
-          Don't miss your next opportunity. Sign in to stay updated on your
-          professional world.
-        </h6>
-        <Col md={6}>
+      <Container>
+        <Col lg={10} className="loginContainer mt-5 mx-auto">
+          <img src={FooterLogo} className="my-4 py-2" />
+          <h4>Welcome Back</h4>
+          <h6>
+            Don't miss your next opportunity. Sign in to stay updated on your
+            professional world.
+          </h6>
+        </Col>
+        <Col lg={3}>
           <Form>
             <Form.Group>
               <Form.Control
@@ -33,9 +40,24 @@ export default class Login extends Component {
                 onChange={(e) => this.onChangeHandler(e)}
               />
             </Form.Group>
+            <Form.Group>
+              <Form.Control
+                required
+                id="password"
+                value={this.state.user.password}
+                type={this.state.hidden ? "password" : "text"}
+                size="lg"
+                placeholder="Password"
+                onChange={(e) => this.onChangeHandler(e)}
+              />
+              <button onClick={(e) => this.toggleShow(e)}>
+                {this.state.hidden ? "Show" : "Hide"}
+              </button>
+            </Form.Group>
           </Form>
+          <Button className="loginBtn">Sign in</Button>
         </Col>
-      </Col>
+      </Container>
     );
   }
 }
