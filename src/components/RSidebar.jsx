83318302c09416febe2d8143bf75b810f2da344c
nostam <matsonho@gmail.com>
2020-12-03 21:30:44 +0100

    add logout function

--- src/components/RSidebar.jsx
+++ src/components/RSidebar.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { Image, Card, ListGroup, ListGroupItem } from "react-bootstrap";
+import { Image, Card, ListGroup, ListGroupItem, Button } from "react-bootstrap";
+import { withRouter } from "react-router-dom";
 
-export default class RSidebar extends React.Component {
+class RSidebar extends React.Component {
   state = {
     Me: [],
   };
-
+  logout = () => {
+    localStorage.setItem("token", "");
+    this.props.history.push("/");
+  };
   async componentDidMount() {
     try {
       const MeFetch = await fetch(
@@ -46,9 +50,15 @@ export default class RSidebar extends React.Component {
             <ListGroupItem action>Connections</ListGroupItem>
             <ListGroupItem action>See all Premium Features</ListGroupItem>
             <ListGroupItem action>My Items</ListGroupItem>
+            <ListGroupItem onClick={() => this.logout()}>
+              <Button size="sm" variant="outline-danger">
+                Logout
+              </Button>
+            </ListGroupItem>
           </ListGroup>
         </Card>
       </>
     );
   }
 }
+export default withRouter(RSidebar);
