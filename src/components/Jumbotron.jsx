a8efb0c2822b2bdc79ae321c0318af0dca92760e
Lidia Kovac <lidia.kovac1998@gmail.com>
2020-12-03 12:04:47 +0100

    adds responsive btn

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -12,25 +12,30 @@ class Header extends React.Component {
       >
         <div className="coverpic"></div>
         {/*Edit, more and add section button*/}
-        <div className="wrapper">
+        <div className="wrapper" >
           <Row
             style={{
               position: `absolute`,
-              top: `10vw`,
+              top: `17vh`,
               right: `3.5vw`,
-              width: '18vw'
+              width: "18vw",
             }}
           >
-            <Col sm={12} lg={7} xl={7} style={{paddingLeft: '.5vw', paddingRight: '.5vw'}} >
+            <Col
+              sm={12}
+              lg={7}
+              xl={7}
+              style={{ paddingLeft: ".5vw", paddingRight: ".5vw" }}
+            >
               <Dropdown>
                 <Dropdown.Toggle
                   className="rounded-pill"
                   id="dropdown-basic"
                   style={{
-                    width: '10vw',
+                    width: "10vw",
                     overflow: "hidden",
-                    whiteSpace: 'nowrap',
-                    textOverflow: 'ellipsis',
+                    whiteSpace: "nowrap",
+                    textOverflow: "ellipsis",
                     backgroundColor: "#0A66CE",
                     height: "3.1vh",
                     fontWeight: 600,
@@ -53,16 +58,16 @@ class Header extends React.Component {
                 </Dropdown.Menu>
               </Dropdown>
             </Col>
-            <Col sm={12} lg={3} xl={3} >
+            <Col sm={12} lg={3} xl={3}>
               <Button
                 style={{
-                  width: '4.5vw',
+                  width: "4.5vw",
                   overflow: "hidden",
-                  whiteSpace: 'nowrap',
-                  textOverflow: 'ellipsis',
+                  whiteSpace: "nowrap",
+                  textOverflow: "ellipsis",
                   height: "2.8vh",
                   paddingBottom: "1.5vw",
-                  overflow: "hidden"
+                  overflow: "hidden",
                 }}
                 className="rounded-pill"
                 variant="outline-secondary"
@@ -70,7 +75,7 @@ class Header extends React.Component {
                 More...
               </Button>
             </Col>
-            <Col sm={12} lg={2} xl={2} >
+            <Col sm={12} lg={2} xl={2}>
               <EditPage
                 profile={this.props.profile}
                 refetch={this.props.refetch}
@@ -85,8 +90,13 @@ class Header extends React.Component {
               <div id="headerName">{this.props.name}</div>
             </Col>
             <Col>
-              <img src={Job} className="job" />
-              <div className="jobdec hoverBlue">{this.props.desc}</div>
+             <Col>
+             <img src={Job} className="job" />
+             </Col> 
+             <Col>
+             <div className="jobdec hoverBlue">{this.props.desc}</div>
+             </Col>
+              
             </Col>
           </Row>
           <Row className="d-flex" noGutters>
@@ -100,8 +110,8 @@ class Header extends React.Component {
               ∙&nbsp;&nbsp;24 connections&nbsp;&nbsp;∙&nbsp;&nbsp;Contact Info
             </div>
           </Row>
-
-          <Card
+         {/* <Row
+            className="d-flex opentowork"
             style={{
               width: "38vw",
               height: "6.8vw",
@@ -110,40 +120,45 @@ class Header extends React.Component {
               borderRadius: ".5vw",
             }}
           >
-            <Card.Header
-              className="hoverBlue"
-              style={{ fontSize: "11pt", borderRadius: ".5vw" }}
-            >
-              <strong>Open to work</strong> <br />
-              {this.props.desc} roles <br />
-              <strong style={{ color: "#087BBA" }}>See all details</strong>
-            </Card.Header>
-            <Card.Footer
-              style={{
-                backgroundColor: "white",
-                fontSize: "11pt",
-                height: `3vh`,
-                padding: `.2vw`,
-                paddingLeft: `1vw`,
-              }}
-            >
-              <svg
-                width="1em"
-                height="1em"
-                viewBox="0 0 16 16"
-                class="bi bi-eye-fill m-1"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
-                <path
-                  fill-rule="evenodd"
-                  d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"
-                />
-              </svg>
-              Only recruiters
-            </Card.Footer>
-          </Card>
+            <Col xs={12}>
+              <Card>
+                <Card.Header
+                  className="hoverBlue"
+                  style={{ fontSize: "11pt", borderRadius: ".5vw" }}
+                >
+                  <strong>Open to work</strong> <br />
+                  {this.props.desc} roles <br />
+                  <strong style={{ color: "#087BBA" }}>See all details</strong>
+                </Card.Header>
+                <Card.Footer
+                  style={{
+                    backgroundColor: "white",
+                    fontSize: "11pt",
+                    height: `3vh`,
+                    padding: `.2vw`,
+                    paddingLeft: `1vw`,
+                  }}
+                >
+                  <svg
+                    width="1em"
+                    height="1em"
+                    viewBox="0 0 16 16"
+                    class="bi bi-eye-fill m-1"
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+                    <path
+                      fill-rule="evenodd"
+                      d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"
+                    />
+                  </svg>
+                  Only recruiters
+                </Card.Footer>
+              </Card>
+            </Col>
+          </Row>
+                */}
         </div>
       </Jumbotron>
     );
