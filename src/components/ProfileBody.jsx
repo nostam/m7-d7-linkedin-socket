f4efac436da2a73dc59480f2c5fdb78062ee04b0
lucaperullo <72197904+lucaperullo@users.noreply.github.com>
2020-12-04 02:50:10 +0100

    up

--- src/components/ProfileBody.jsx
+++ src/components/ProfileBody.jsx
@@ -53,71 +53,75 @@ class Body extends React.Component {
   };
   render() {
     return (
-      <div className="mainBody">
-        {this.state.err && <Alert variant="danger">{this.state.errMsg}</Alert>}
-        {this.state.loading && this.state.err !== true ? (
-          <Row className="d-flex justify-content-center my-5">
-            <h3 style={{ paddingTop: "20vh" }}>Loading profile...</h3>
-            <Spinner
-              animation="border"
-              variant="info"
-              style={{ marginTop: "20vh" }}
-            />
-          </Row>
-        ) : Object.keys(this.state.profile).length !== 0 ? (
-          <Row className="rowm">
-            {/*<Col lg={3}></Col> */}
-            <Col md={8} style={{ marginTop: "10vh" }}>
-              <Card>
-                <Card.Img
-                  className="cardImg"
-                  variant="top"
-                  src="https://coverfiles.alphacoders.com/372/37275.jpg"
-                  style={{ objectFit: "cover" }}
-                  alt="placeholderr"
-                />
-                <Card.Body>
-                  <div className="d-flex justify-content-between">
-                    <div style={{ marginTop: "-130px" }}>
-                      <img
-                        src={this.state.profile.image}
-                        alt="placeholder"
-                        height="160px"
-                        width="160px"
-                        style={{
-                          borderRadius: "50%",
-                          border: "4px solid white",
-                          objectFit: "cover",
-                        }}
-                      ></img>
+      <div className="bgBody">
+        <div className="mainBody">
+          {this.state.err && (
+            <Alert variant="danger">{this.state.errMsg}</Alert>
+          )}
+          {this.state.loading && this.state.err !== true ? (
+            <Row className="d-flex justify-content-center my-5">
+              <h3 style={{ paddingTop: "20vh" }}>Loading profile...</h3>
+              <Spinner
+                animation="border"
+                variant="info"
+                style={{ marginTop: "20vh" }}
+              />
+            </Row>
+          ) : Object.keys(this.state.profile).length !== 0 ? (
+            <Row className="rowm">
+              {/*<Col lg={3}></Col> */}
+              <Col md={8} style={{ marginTop: "10vh" }}>
+                <Card>
+                  <Card.Img
+                    className="cardImg"
+                    variant="top"
+                    src="https://coverfiles.alphacoders.com/372/37275.jpg"
+                    style={{ objectFit: "cover" }}
+                    alt="placeholderr"
+                  />
+                  <Card.Body>
+                    <div className="d-flex justify-content-between">
+                      <div style={{ marginTop: "-130px" }}>
+                        <img
+                          src={this.state.profile.image}
+                          alt="placeholder"
+                          height="160px"
+                          width="160px"
+                          style={{
+                            borderRadius: "50%",
+                            border: "4px solid white",
+                            objectFit: "cover",
+                          }}
+                        ></img>
+                      </div>
                     </div>
-                  </div>
 
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-              <Bio bio={this.state.profile.bio} />
-              <Feature />
-              <Experience profile={this.state.profile} />
-            </Col>
-            <Col
-              md={4}
-              style={{ marginTop: "10vh" }}
-              className="d-none d-md-block"
-            >
-              <Sidebar />
-            </Col>
-          </Row>
-        ) : (
-          this.setState({
-            err: true,
-            errType: "warning",
-            errMsg: "We have encounter a problem, the profile is empty",
-          })
-        )}
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+                <Bio bio={this.state.profile.bio} />
+                <Feature />
+                <Experience profile={this.state.profile} />
+              </Col>
+              <Col
+                md={4}
+                style={{ marginTop: "10vh" }}
+                className="d-none d-md-block"
+              >
+                <Sidebar />
+              </Col>
+            </Row>
+          ) : (
+            this.setState({
+              err: true,
+              errType: "warning",
+              errMsg: "We have encounter a problem, the profile is empty",
+            })
+          )}
+        </div>
       </div>
     );
   }
