95cffec668691360ca4ccd115245a6f50f004a79
Lidia Kovac <lidia.kovac1998@gmail.com>
2020-12-04 10:12:59 +0100

    adds route

--- src/components/BioCard.jsx
+++ src/components/BioCard.jsx
@@ -4,6 +4,7 @@ import "../App.css";
 import { Card, Col, Row } from "react-bootstrap";
 import EditPage from "./EditPage";
 import "../styles/Profile.css";
+import { Route } from "react-router-dom";
 
 class Bio extends React.Component {
   render() {
@@ -13,11 +14,13 @@ class Bio extends React.Component {
           <Row className="d-flex justify-content-between ml-1">
             <div className="info">About</div>
 
-            <EditPage
+            <Route path='/user/me'>  <EditPage
               profile={this.props.profile}
               refetch={this.props.refetch}
               color="#0A66CE"
             />
+            </Route>
+            
           </Row>
 
           <div class="infobody">{this.props.bio}</div>
